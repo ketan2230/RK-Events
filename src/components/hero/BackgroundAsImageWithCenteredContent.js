c31919a5675c23c2b6f1f1a1719bfd0edb5de3a0
prince <princekansagra1@gmail.com>
2023-05-14 17:11:48 +0530

    new change

--- src/components/hero/BackgroundAsImageWithCenteredContent.js
+++ src/components/hero/BackgroundAsImageWithCenteredContent.js
@@ -1,8 +1,11 @@
 import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
-import { useState, useEffect } from 'react';
-import Data from "data/events.json"
+import { useState, useEffect } from "react";
+import Data from "data/events.json";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 import Header, {
   NavLink,
@@ -14,7 +17,7 @@ import Header, {
 } from "../headers/light.js";
 
 const StyledHeader = styled(Header)`
-  ${tw`pt-5 max-w-none w-full`}
+  ${tw`pt-5 max-w-none w-full absolute px-5 z-30`}
   ${DesktopNavLinks} ${NavLink}, ${LogoLink} {
     ${tw`text-gray-100 hover:border-gray-300 hover:text-gray-300`}
   }
@@ -28,10 +31,10 @@ const Container = styled.div`
   ${tw`relative -mx-8 -mt-8 bg-center bg-cover h-screen min-h-144`}
 `;
 
-const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-black opacity-75`;
+const OpacityOverlay = tw.div`absolute inset-0 bg-black opacity-75`;
 
-const HeroContainer = tw.div`z-20 relative px-6 sm:px-8 mx-auto h-full flex flex-col`;
-const Content = tw.div`px-4 flex flex-1 flex-col justify-center items-center w-2/3 mx-auto`;
+const HeroContainer = tw.div`relative mx-auto h-full flex flex-col`;
+const Content = tw.div`px-4 flex flex-1 flex-col justify-center items-center w-full`;
 
 const Heading = styled.h1`
   ${tw`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl font-black text-gray-100 leading-snug -mt-24 sm:mt-0`}
@@ -61,37 +64,67 @@ export default ({
     </NavLinks>,
   ];
 
-  const bgImages = Data.mainPageImagesSlogan
+  const bgImages = Data.mainPageImagesSlogan;
 
-  const [bgIndex, setBgIndex] = useState(0);
-  const currentBgImage = bgImages[bgIndex % bgImages.length];
-
-  const style = {
-    backgroundImage: `url(${currentBgImage.imageSrc})`,
-    transition: 'opacity 4s ease-in-out',
-    opacity: 1,
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBgIndex((prevIndex) => prevIndex + 1);
-    }, 7000);
-  
-    return () => clearInterval(interval);
-  }, []);
+  const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  const optionsOwl = {
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplay: 7000,
+    responsiveClass: true,
+  };
 
   return (
-    <Container style={style}>
-      <OpacityOverlay />
+    <Container>
       <HeroContainer>
         <StyledHeader links={navLinks} />
-        <Content>
-          <Heading>
-            {currentBgImage.slogan}
-          </Heading>
-          {/* <PrimaryAction>Explore Themes</PrimaryAction> */}
-        </Content>
+        <OwlCarousel className="owl-theme" {...optionsOwl}>
+          {bgImages.map((item) => {
+            return (
+              <div class="item">
+                <OpacityOverlay />
+                <Content
+                  style={{
+                    backgroundImage: `url(${item.imageSrc})`,
+                    transition: "opacity 4s ease-in-out",
+                    opacity: 1,
+                    backgroundSize: "cover",
+                  }}
+                >
+                  <Heading
+                    style={{
+                      height: "100vh",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      zIndex: 99,
+                    }}
+                  >
+                    {item.slogan}
+                  </Heading>
+                  {/* <PrimaryAction>Explore Themes</PrimaryAction> */}
+                </Content>
+              </div>
+            );
+          })}
+        </OwlCarousel>
       </HeroContainer>
     </Container>
   );
