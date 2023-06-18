8845550219dbaf840f5a294186e88a0386aad3ec
pratik-divii <pratik@divii.com>
2023-06-18 12:10:58 +0530

    Decrease load time while switching tab.

--- src/components/headers/MainStickyHeader.js
+++ src/components/headers/MainStickyHeader.js
@@ -77,6 +77,37 @@ const navLinks = [
   </NavLinks>,
 ];
 
+(function() {
+	var anchors = document.getElementsByTagName('a'); //Find all links
+	var historyState = {};
+	//We will use the current "folder" as the base, so all new locations are relative to that
+	// var base = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1);
+	
+	//Go through all the links and change the relevant ones
+	for (var i = 0; i < anchors.length; i++) {
+		//Only apply this to same domain links
+		if (anchors[i].href.substring(0, window.location.origin.length) !== window.location.origin) {
+			continue;
+		}
+		
+		anchors[i].addEventListener('click', function(evt) {
+			//When clicked, we do not want the link to be processed normally
+			evt.preventDefault();
+			
+			//Change to new page with hash
+			// var newPage = window.location.href + '#' + evt.target.href.replace(base, '');
+			// window.location.href = newPage;
+			
+			//Only do this if history.pushState is supported by the browser
+			if (window.history && window.history.pushState) {
+				//Remove hash from URL and replace with desired URL
+				window.history.pushState(historyState, evt.target.innerHTML, evt.target.href)
+			}
+		});
+	}
+})();
+
+
 const MainStickyHeader = () => {
   return (
     <HeaderContainer>
