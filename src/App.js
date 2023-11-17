47de899c2200d57545d8b00649a63c1ac888d343
ketan2230 <ketanvaghela2230@gmail.com>
2023-11-17 16:35:25 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const Footer = lazy(() => import('./components/footers/MiniCenteredFooter'));
 const GoToTop = lazy(() => import('./components/footers/GoToTop'));
 const Loader = lazy(() => import('./components/common/Loader'));
 const AppRoutes = lazy(() => import('./components/common/Routes'));
-const GlobalStyles = lazy(() => import('./styles/GlobalStyles'));
+const GlobalStyles = lazy(() => import('./style/GlobalStyles'));
 
 
 export default function App() {
